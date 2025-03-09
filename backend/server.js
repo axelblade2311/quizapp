65b9ed9e3e025f13601eb5b513313bb3dba01d05
axelblade2311 <dhanuknikhil2311@gmail.com>
2025-03-09 20:00:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use("/api/auth",adminRouter);
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
     });
 }
 ).catch((error)=>{  
