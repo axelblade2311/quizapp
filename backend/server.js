9fcd9b30c29e6bfef7d60b54d401a17e0a730486
axelblade2311 <dhanuknikhil2311@gmail.com>
2025-03-09 19:57:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth",adminRouter);
-
 connectDB().then(()=>{
     app.listen(process.env.PORT,()=>{
         console.log(`Server is running on port ${process.env.PORT}`);
