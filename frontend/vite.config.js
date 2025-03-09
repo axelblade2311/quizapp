753a9e395ff780b604fd59f7804fb46eb8df68b2
axelblade2311 <dhanuknikhil2311@gmail.com>
2025-03-09 20:40:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/quizapp/", // Change "quizapp" to your GitHub repo name
   plugins: [react()],
+  base: './', // Ensures correct asset paths
 });
